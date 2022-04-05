b242cd9a5cb68204f637e95d772f9633def49174
Александр Морев <a.morev@kryptonite.ru>
2022-04-06 00:52:51 +0300

    Add front

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,18 @@
-from django.http import HttpResponse
+from re import template
+from django.shortcuts import render
 
 
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = "posts/index.html"
+    context = {
+        'title': "Это главная страница проекта Yatube",
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группы {slug}')
+    template = "posts/group_list.html"
+    context = {
+        'title': "Здесь будет информация о группах проекта Yatube",
+    }
+    return render(request, template, context)
